8ed335eeac0ff41faf215dc532f41a0658548262
Lisber Victores <lvpompa@gmail.com>
2021-05-26 07:19:27 -0300

    Project created

---
+++ backend/src/test/java/com/lisbervictores/myinvest/MyInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisbervictores.myinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
